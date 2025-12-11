246f7f1035c8fbf07d8274aae203359e1ff6e6c4
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2025-12-11 18:13:06 +0100

    refactor: reorganize project

--- tasks/prepare-deployment.mjs
+++ tasks/prepare-deployment.mjs
@@ -7,28 +7,28 @@ const productionUrl = `https://lehrgrapht.de/addin/${packageJson.version}`;
 
 const copyManifestFile = () =>
   function copyManifestFile() {
-    return gulp.src('manifest.xml').pipe(gulp.dest('dist/lehrgrapht/browser'));
+    return gulp.src('manifest.xml').pipe(gulp.dest('dist/addin/browser'));
   };
 
 const replaceDeploymentUrl = () =>
   function replaceDeploymentUrl() {
     return gulp
-      .src('dist/lehrgrapht/browser/manifest.xml')
+      .src('dist/addin/browser/manifest.xml')
       .pipe(replace(localUrl, productionUrl))
-      .pipe(gulp.dest('dist/lehrgrapht/browser'));
+      .pipe(gulp.dest('dist/addin/browser'));
   };
 
 const replaceVersion = () =>
   function replaceVersion() {
     return gulp
-      .src('dist/lehrgrapht/browser/manifest.xml')
+      .src('dist/addin/browser/manifest.xml')
       .pipe(
         replace(
           '<Version>1.0.0.0</Version>',
           `<Version>${packageJson.version}</Version>`,
         ),
       )
-      .pipe(gulp.dest('dist/lehrgrapht/browser'));
+      .pipe(gulp.dest('dist/addin/browser'));
   };
 
 export default gulp.series(
