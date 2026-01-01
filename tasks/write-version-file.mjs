22a4d410739dac3c0feeda525e0ef0b3ed8df747
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2026-01-01 14:53:15 +0100

    build: update version file

--- tasks/write-version-file.mjs
+++ tasks/write-version-file.mjs
@@ -1,12 +1,12 @@
 import gulp from 'gulp';
 import { writeFile } from 'node:fs/promises';
-import packageJson from '../package.json';
+import packageJson from '../package.json' with { type: 'json' };
 
 const version = process.env.APP_VERSION || packageJson.version;
 
 const writeVersionTsFile = () =>
   async function writeVersionTsFile(done) {
-    const versionTsContent = `export const lehrgraphtVersion = '${version}';'`;
+    const versionTsContent = `export const lehrgraphtVersion = '${version}';`;
     await writeFile('projects/addin/src/version.g.ts', versionTsContent);
     done();
   };
