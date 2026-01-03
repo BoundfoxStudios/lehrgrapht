ff6b709c921dced08da569a026bbae9274d73f66
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2026-01-03 18:35:30 +0100

    feat: add website meta data
    fixes #15

--- projects/website/src/app/pages/home-page/home-page.ts
+++ projects/website/src/app/pages/home-page/home-page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Hero } from '../../components/hero/hero';
 import { ContentContainer } from '../../components/content-container/content-container';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
@@ -10,6 +10,7 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faMicrosoft } from '@fortawesome/free-brands-svg-icons';
 import { KnownIssues } from '../../components/known-issues/known-issues';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'lgw-home-page',
@@ -24,4 +25,31 @@ export class HomePage {
   protected readonly faBoltLightning = faBoltLightning;
   protected readonly faStar = faStar;
   protected readonly faMicrosoft = faMicrosoft;
+
+  private readonly title = inject(Title);
+  private readonly meta = inject(Meta);
+
+  constructor() {
+    const title = 'LehrGrapht - Mathe-Plotter-Word-AddIn für Lehrkräfte';
+    const description =
+      'Ein kostenfreies Word AddIn zum Erstellen von maßstabsgetreuen Plots für Lehrkräfte.';
+
+    this.title.setTitle(title);
+    this.meta.addTag({
+      property: 'og:title',
+      content: title,
+    });
+    this.meta.addTag({
+      name: 'description',
+      content: description,
+    });
+    this.meta.addTag({
+      property: 'og:description',
+      content: description,
+    });
+    this.meta.addTag({
+      property: 'og:url',
+      content: 'https://lehrgrapht.de',
+    });
+  }
 }
--- projects/website/src/app/pages/privacy-policy-page/privacy-policy-page.ts
+++ projects/website/src/app/pages/privacy-policy-page/privacy-policy-page.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Hero } from '../../components/hero/hero';
 import { ContentContainer } from '../../components/content-container/content-container';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'lgw-privacy-policy-page',
@@ -8,4 +9,31 @@ import { ContentContainer } from '../../components/content-container/content-con
   templateUrl: './privacy-policy-page.html',
   styleUrl: './privacy-policy-page.css',
 })
-export class PrivacyPolicyPage {}
+export class PrivacyPolicyPage {
+  private readonly title = inject(Title);
+  private readonly meta = inject(Meta);
+
+  constructor() {
+    const title = 'LehrGrapht - Datenschutzerklärung';
+    const description =
+      'Die Datenschutzerklärung von LehrGrapht - wir sammeln keinerlei Daten!';
+
+    this.title.setTitle(title);
+    this.meta.addTag({
+      property: 'og:title',
+      content: title,
+    });
+    this.meta.addTag({
+      name: 'description',
+      content: description,
+    });
+    this.meta.addTag({
+      property: 'og:description',
+      content: description,
+    });
+    this.meta.addTag({
+      property: 'og:url',
+      content: 'https://lehrgrapht.de/privacy-policy',
+    });
+  }
+}
