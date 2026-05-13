fe229a16e1fc60c668b5afd2d51485300767fbe8
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2026-05-13 09:12:58 +0200

    feat(addin): register toggleSolutionView Office action after bootstrap

--- projects/addin/src/main.ts
+++ projects/addin/src/main.ts
@@ -2,13 +2,19 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/components/app/app';
 import { createAppConfig } from './app/app.config';
 import { RunConfiguration } from './app/models/run-configuration';
+import { SolutionViewService } from './app/services/solution-view.service';
 
 const bootstrap = (runConfiguration: RunConfiguration): void => {
-  bootstrapApplication(App, createAppConfig(runConfiguration)).catch(
-    (err: unknown) => {
+  bootstrapApplication(App, createAppConfig(runConfiguration))
+    .then(appRef => {
+      const solutionView = appRef.injector.get(SolutionViewService);
+      Office.actions.associate('toggleSolutionView', () => {
+        void solutionView.toggle();
+      });
+    })
+    .catch((err: unknown) => {
       console.error(err);
-    },
-  );
+    });
 };
 
 void Office.onReady(info => {
